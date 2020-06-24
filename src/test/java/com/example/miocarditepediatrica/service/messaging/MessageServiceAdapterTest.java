6118b3b65ca2902156d3e0290cb5db3c0f7680e1
Daniel Ye <daniel.ye@m4u.com.br>
2020-06-24 08:29:40 -0300

    adicionando teste de criacao de medico

--- src/test/java/com/example/miocarditepediatrica/service/messaging/MessageServiceAdapterTest.java
+++ src/test/java/com/example/miocarditepediatrica/service/messaging/MessageServiceAdapterTest.java
@@ -7,6 +7,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -19,11 +20,21 @@ public class MessageServiceAdapterTest {
     DoctorServicePort doctorServicePort;
 
     @Test
-    public void deveria_receber_tratar_payload_corretamente(){
+    public void deveria_tratar_payload_de_delecao_corretamente(){
         String payload = "{\"status\":\"delete\",\"medico\":{\"id\": \"1234\", \"nome\": null, \"crm\": null, \"especialidade\": null, \"dataDeNascimento\": null}}";
 
         messageServiceAdapter.processDoctorMessage(payload);
 
         verify(doctorServicePort).deleteDoctor("1234");
+    }
+
+
+    @Test
+    public void deveria_tratar_payload_de_criacao_corretamente(){
+        String payload = "{\"status\":\"create\",\"medico\":{\"id\": \"1234\", \"nome\": \"fulano\", \"crm\": \"1242424\", \"especialidade\": \"cardiologia\", \"dataDeNascimento\": \"01-01-2000\"}}";
+
+        messageServiceAdapter.processDoctorMessage(payload);
+
+        verify(doctorServicePort).addDoctor(any());
     }
 }
