44823b3319bccfee1c95fdafbc6c4608d07423f1
Daniel Ye <danielxinxin1@gmail.com>
2020-11-26 15:32:39 -0300

    corrigindo nome de arvore de decisao

--- src/main/java/com/example/miocarditepediatrica/domain/decisiontree/DecisionTreeNode.java
+++ src/main/java/com/example/miocarditepediatrica/domain/decisiontree/DecisionTreeNode.java
@@ -7,4 +7,46 @@ public class DecisionTreeNode {
     private String type;
     private String text;
     private List<String> possibleValues;
+
+    public DecisionTreeNode(String id, String type, String text, List<String> possibleValues) {
+        this.id = id;
+        this.type = type;
+        this.text = text;
+        this.possibleValues = possibleValues;
+    }
+
+    public DecisionTreeNode() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<String> getPossibleValues() {
+        return possibleValues;
+    }
+
+    public void setPossibleValues(List<String> possibleValues) {
+        this.possibleValues = possibleValues;
+    }
 }
--- src/main/java/com/example/miocarditepediatrica/out/controller/DecisionTreeController.java
+++ src/main/java/com/example/miocarditepediatrica/out/controller/DecisionTreeController.java
@@ -8,16 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import javax.validation.Valid;
-
 @RestController
 @RequestMapping("/tree")
 @CrossOrigin
 public class DecisionTreeController {
 
     @RequestMapping(value = "/check", method = RequestMethod.POST)
-    public ResponseEntity showTest(@Valid DecisionTreeAnswer answer){
-        answer.setTreeName("miocardite-pediatrica");
+    public ResponseEntity showTest(@RequestBody DecisionTreeAnswer answer){
         RestTemplate template = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
